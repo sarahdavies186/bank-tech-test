3d232b71ad81fc51a415b2356408a992dc38d526
Sarah <sarahdavies186@gmail.com>
2023-04-17 14:27:43 +0100

    third test passing, multiple deposits updating balance correctly

--- bankStatement.test.js
+++ bankStatement.test.js
@@ -1,15 +1,24 @@
-const BankStatement = require('./bankStatement')
+const BankStatement = require("./bankStatement");
 
-describe('BankStatement class', () => {
-  const bankStatement = new BankStatement();
+describe("BankStatement class", () => {
+  let bankStatement 
+
+  beforeEach(() => {
+    bankStatement = new BankStatement();
+  });
   
-  it('starts with an inital balance of 0', () => {
-  expect(bankStatement.getBalance()).toBe(0)
-  })
+  it("starts with an inital balance of 0", () => {
+    expect(bankStatement.getBalance()).toBe(0);
+  });
 
-  it('increases the balance when a deposit is made', () => {
-    bankStatement.makeDeposit(1000.00);
-    expect(bankStatement.getBalance()).toBe(1000.00)
-    })
+  it("increases the balance when a deposit is made", () => {
+    bankStatement.makeDeposit(1000.0);
+    expect(bankStatement.getBalance()).toBe(1000.0);
+  });
 
-})
+  it("increases the balance when 2 deposits are made", () => {
+    bankStatement.makeDeposit(1000.0);
+    bankStatement.makeDeposit(2000.0);
+    expect(bankStatement.getBalance()).toBe(3000.0);
+  });
+});
