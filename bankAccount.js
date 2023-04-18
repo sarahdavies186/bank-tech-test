a203624750b063e11795710c03c22d88a2f4af7f
Sarah <sarahdavies186@gmail.com>
2023-04-18 11:06:42 +0100

    deleted duplicate code

--- bankAccount.js
+++ bankAccount.js
@@ -12,26 +12,10 @@ class BankAccount{
 
   makeDeposit(amount, date) { //date has to be a string
     this.addTransaction(amount, date, true)
-    // if (typeof int === 'number' && !isNaN(int) && !isNaN(parseFloat(int))) {
-    //   const formattedInt = parseFloat(int.toFixed(2));
-    //   this.balance = this.balance + formattedInt;
-    //   const statement = `${date} || ${formattedInt.toFixed(2)} || || ${this.balance.toFixed(2)}`
-    //   this.statement.push(statement)
-    // } else {
-    //   throw new Error('Input should be an integer');
-    // }
   }
 
   makeWithdrawal(amount, date) { //date has to be a string
     this.addTransaction(amount, date, false)
-    // if (typeof int === 'number' && !isNaN(int) && !isNaN(parseFloat(int))) {
-    //   const formattedInt = parseFloat(int.toFixed(2));
-    //   this.balance = this.balance - formattedInt;
-    //   const statement = `${date} || || ${formattedInt.toFixed(2)} || ${this.balance.toFixed(2)}`
-    //   this.statement.push(statement)
-    // } else {
-    //   throw new Error('Input should be an integer');
-    // }
   }
 
   addTransaction(amount, date, isDeposit) {
